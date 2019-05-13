e4ea4171efd7601008bfc3f0328abeb954e8ca00
Kari.Fenske <Kari.Fenske@nmfs.local>
2019-05-13 09:45:12 -0800

    adding two data spreadsheets (movement and age-2 proportions by area) and updating the run-sf-sim to have things in the operational order I think they need to be in

--- Run-Sablefish-Sim.R
+++ Run-Sablefish-Sim.R
@@ -72,15 +72,16 @@ create_sim_recruitments(mu_rec=mu_rec, sigma_rec=sigma_rec, rho_rec=NULL,
 # divide annual recruitments into areas 
 for(i in 1:n.sims){
 for(y in 1:n.year){
-  recruits.area[y,,i] <- spatial_rec(rec[i,y],area.props=c(1,1,1,1,1,1), ss=100, seed=1)      # this needs thought about sex distribution of recruitment.
+  recruits.area[y,,i] <- spatial_rec(rec[i,y],area.props=c(0.14,0.07,0.14,0.43,0.14,0.09), ss=100, seed=1)      # this needs thought about sex distribution of recruitment.
 }}
 
 
 # Initialize Population (year 1, or change to years 1-X) =============================================================
-#   Should probably update to start from FISHED equilibrium (and spatially mixed equilibrium??) or set to stable distribution of movement proportions...
-#   HOW DO WE DO THAT? WOULD WE NEED A SEPARATE FUNCTION TO COME UP WITH EQUILIB THEN READ IT IN HERE BY AREA?
+#   Should probably update to start from FISHED equilibrium (and spatially mixed equilibrium??) or set to stable 
+#   distribution of movement proportions...
 #   this sets up the population proportions for year 1 for each of the N number of simulations.
 #   I think we need to add code here, starting with init.prop, to run forward ~50-60 years to make a fished equilib...
+#   and build our initial .dat file (summed across n.areas), us F generated from actual observed catches
 init.prop <- calc_init_age_prop(bo=mu_rec)
 
 i <- 1
@@ -102,25 +103,22 @@ for(i in 1:n.sims) {
   y <- 2  
   for(y in 2:n.year) {
     
-    
-    
     m <- 1
     for (m in 1:n.area) {
-    #Initial Recruitment (<- is this a bad section name? I don't understand why it's called this)
-    ssb[,,y-1,m,i] <- ma*wa*N[,y-1,,m,i] #ssb dims = n.sex, n.age, n.year, n.area, n.sims; N dims = n.sex, n.year, n.age, n.area, n.sims
-
-    #HARVEST CONTROL RULE
-    temp.ssb <- sum(ssb[,,y-1,m,i]) #presumably this needs an actual HCR coded in??  Also, why is this here and not somewhere else?
-    
+     
+    # Forward Simulation =============================================================
+    # Temporarily Assuming no movement, need to add that in 
+      
+    #take most recent assessment and outputted ABC apportionment and calculate F 
+    #apply F from the most recent "REAL" stock assessment (spatial model) to OM population (assume perfect execution)- update B, N, SSB
     # need to get an F to use for below
-    
     f <- 1
     for(f in 1:n.fish) {
       # UPDATE: Instead of setting a fixed F, we can now set a fixed catch
       #  biomass and find the F for the specific fishery
       #   that will match that catch
       
-      # Here lets specify an arbitrary fixed catch (kg)
+      # Here lets specify an arbitrary fixed catch (kg) -- will need to eventually read it in 
       catch <- 1e6 # 1 million kg.
       
       # Find Fishing Mortality Rate for Apportioned Catch Level ------------------------
@@ -129,47 +127,10 @@ for(i in 1:n.sims) {
                                            temp.N=N[,y-1,,m,i], 
                                            wa=wa, mx=mx, 
                                            bisection=TRUE)$Fmort
-      
-      
-      
-      Fmort[f,y,m,i] <- 0.1#HCR_linear(curr.SSB=temp.ssb, SSB0=SSB0, floor.F=floors[g], ceiling.F=ceilings[g], 
+      Fmort[f,y,m,i] <- temp.Fmort  #0.1#HCR_linear(curr.SSB=temp.ssb, SSB0=SSB0, floor.F=floors[g], ceiling.F=ceilings[g], 
                                  # ascent.range=ascent.range, plot=FALSE)
     }#next g (g for gear?)
-    
-    #No Recruitment relationship  Can we change this so it reads in a rec value from a separate file which draws N simulations * N years worth of rec values all 
-    # at once so the same recruitment can be applied to single and spatial models? Could make it so that if SSB is 0, 0 rec is used instead so we 
-    # don't spontaneously generate fish if the pop crashes.
-    #rec[,y-1,m,i] <- 0.5 * exp(mu_rec*(1+mx[,1]) + rnorm(1, 0, sigma_rec)) #Note this has the addition to account for assessment model predicting recruitment to age 2. 
-    
-    #Ricker
-    # rec[,y-1] <- 0.5 * ricker_recruit(ssb[y-1], steep, bo)
-    #Beverton-Holt
-    # rec[,y-1,i] <-  0.5 * beverton_holt_recruit(sum(ssb[,,y-1,i]), steep, bo=ro) * exp(rnorm(1,0,sigma_rec) - ((sigma_rec^2)/2))
-    
-    #1) Generate Assessment Data:
-    # insert that code here for: where do these go?  how to sample for these and add error?
-    # observed catch (based on what for F?), 'current' year, for 6 areas then combine to 3 and to 1 (or how do we want to do this so it reads into a single area and a spatial model?)
-    # longline survey RPN, 'current' year, for 6 areas then combine to 3
-    # longline/fixed gear fishery CPUE/RPW, lagged 1 year, for 6 areas then combine to 3
-    # longline/fixed gear fishery age comps, lagged 1 year, for 6 areas then combine to 3, single sex
-    # longline survey age comps, lagged 1 year, for 6 areas then combine to 3, single sex
-    # longline/fixed gear fishery length comps, lagged 1 year, for 6 areas then combine to 3, two sexes
-    # trawl gear fishery length comps, lagged 1 year, for 6 areas then combine to 3, two sexes
-    # longline survey length comps, 'current' year, for 6 areas then combine to 3, two sexes
-    
-    # Forward Simulation =============================================================
-    #Temporarily Assuming no movement, need to add that in WHERE AND WHEN WILL FISH MOVE????
-    
-    #=============================================================
-    #### Conduct Assessment #### <- this seems like the wrong place for this??
-    #2) Call ADMB Model
-    #add code here
-    #=============================================================
-    #### Determine SPR ####
-    # doesn't this come out of my EM?
-    #=============================================================
-    #### Set Harvest Limits ####
-    
+        
     a <- 1
     for(a in 1:n.age) {
       #Update Numbers and Biomass Matrix
@@ -206,7 +167,7 @@ for(i in 1:n.sims) {
             temp.Z <- sum(Fmort[,y,m,i]*va[,m,h,a-1]) + mx[h,a-1]
             
             harvest.n[h,y-1,a-1,f,m,i] <- N[h,y-1,a-1,m,i] * (temp.F/temp.Z) * (1-exp(-1*temp.Z))
-
+            
             harvest.b[h,y-1,a-1,f,m,i] <- harvest.n[h,y-1,a-1,f,m,i] * wa[h,a-1]
           }#next gear
         }#next sex
@@ -242,9 +203,63 @@ for(i in 1:n.sims) {
           }#next gear
         }#next sex
       }# If plus age group
+      
     }#next age  
     
+    #calculate SSB for OM pop
+    ssb[,,y-1,m,i] <- ma*wa*N[,y-1,,m,i] #ssb dims = n.sex, n.age, n.year, n.area, n.sims; N dims = n.sex, n.year, n.age, n.area, n.sims
+    
     } #next area
+    
+    ######Sample population for age comps, survey index, etc. 
+    # Generate Assessment Data:
+    # insert that code here for: where do these go?  how to sample for these and add error?
+    # observed catch (based on what for F?), 'current' year, for 6 areas then combine to 3 and to 1 (or how do we want to do this so it reads into a single area and a spatial model?)
+    # longline survey RPN, 'current' year, for 6 areas then combine to 3
+    # longline/fixed gear fishery CPUE/RPW, lagged 1 year, for 6 areas then combine to 3
+    # longline/fixed gear fishery age comps, lagged 1 year, for 6 areas then combine to 3, single sex
+    # longline survey age comps, lagged 1 year, for 6 areas then combine to 3, single sex
+    # longline/fixed gear fishery length comps, lagged 1 year, for 6 areas then combine to 3, two sexes
+    # trawl gear fishery length comps, lagged 1 year, for 6 areas then combine to 3, two sexes
+    # longline survey length comps, 'current' year, for 6 areas then combine to 3, two sexes
+    
+    call some sampling functions here for the above list
+    
+    
+    Add sampled data to .dat file
+    
+    Pass to EM, run EM
+    Get estimated quantities and HCR output 
+    apportion output (via chosen method) 
+    apply apportionment output to OM (start loop over)
+    
+
+    #HARVEST CONTROL RULE
+    #temp.ssb <- sum(ssb[,,y-1,m,i]) #presumably this needs an actual HCR coded in??  Also, why is this here and not somewhere else?
+    # No Recruitment relationship  Can we change this so it reads in a rec value from a separate file which draws N simulations * N years worth of rec values all 
+    # at once so the same recruitment can be applied to single and spatial models? Could make it so that if SSB is 0, 0 rec is used instead so we 
+    # don't spontaneously generate fish if the pop crashes.
+    #rec[,y-1,m,i] <- 0.5 * exp(mu_rec*(1+mx[,1]) + rnorm(1, 0, sigma_rec)) #Note this has the addition to account for assessment model predicting recruitment to age 2. 
+        #Ricker
+    # rec[,y-1] <- 0.5 * ricker_recruit(ssb[y-1], steep, bo)
+    #Beverton-Holt
+    # rec[,y-1,i] <-  0.5 * beverton_holt_recruit(sum(ssb[,,y-1,i]), steep, bo=ro) * exp(rnorm(1,0,sigma_rec) - ((sigma_rec^2)/2))
+    
+
+    
+
+    
+    #=============================================================
+    #### Conduct Assessment #### <- this seems like the wrong place for this??
+    #2) Call ADMB Model
+    #add code here
+    #=============================================================
+    #### Determine SPR ####
+    # doesn't this come out of my EM?
+    #=============================================================
+    #### Set Harvest Limits ####
+    
+
     
     #call a function that generates a .dat file here?
     
