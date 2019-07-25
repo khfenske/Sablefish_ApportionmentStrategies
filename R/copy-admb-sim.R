8e81bc94345d8e9c90f000e14b2f4632e484a82d
Curry Cunningham <cunninghamcurry@gmail.com>
2019-07-25 14:57:55 -0800

    Create function to copy all necessary pieces for a single simulation (realization) into the sim-specific folder.

---
+++ R/copy-admb-sim.R
@@ -0,0 +1,18 @@
+#' Copies Necessary Components Into Simulation-Specific Directory
+#'
+#' @param dir.from Directory components are copied FROM - probably dir.admb
+#' @param dir.to Directory components are copied TO - probably dir.temp
+#'
+#' @return
+#' @export
+#'
+#' @examples
+copy_admb_sim <- function(dir.from=dir.admb, dir.to=dir.temp) {
+  # ADMB Executible
+  file.copy(from=file.path(dir.from,"tem"), to=file.path(dir.to,"tem"))
+  # Control File
+  file.copy(from=file.path(dir.from,"tem.ctl"), to=file.path(dir.to,"tem.ctl"))
+  # Dat File
+  file.copy(from=file.path(dir.from,"tem_single2018.dat"), to=file.path(dir.to,"tem_single2018.dat"))
+  file.copy(from=file.path(dir.from,"permanant_tem_single2018.dat"), to=file.path(dir.to,"permanant_tem_single2018.dat")) # Not really necessary
+}
